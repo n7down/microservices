5abc19658615c026ab8f857cce01de07bc3fa783
Joshua Adams <n7down@gmail.com>
2019-10-20 00:34:29 -0600

    small update

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
-	"os"
+	//"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/n7down/microservices/internal/client/greeter"
 	"github.com/n7down/microservices/internal/client/users"
 	"github.com/n7down/microservices/internal/gateway"
+	"github.com/n7down/microservices/internal/utils"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -19,13 +20,15 @@ func main() {
 	router := gin.Default()
 	log.SetReportCaller(true)
 
-	greeterServer, err := greeter.NewGreeterServer(os.Getenv("GREETER_HOST"))
+	//greeterServer, err := greeter.NewGreeterServer(os.Getenv("GREETER_HOST"))
+	greeterServer, err := greeter.NewGreeterServer(utils.GetEnv("GREETER_HOST", "127.0.0.1:8081"))
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
 
-	usersServer, err := users.NewUsersServer(os.Getenv("USERS_HOST"))
+	//usersServer, err := users.NewUsersServer(os.Getenv("USERS_HOST"))
+	usersServer, err := users.NewUsersServer(utils.GetEnv("USERS_HOST", "127.0.0.1:8082"))
 	if err != nil {
 		log.Fatal(err)
 		return
